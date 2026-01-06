7f0062f01ee498dd85234395c689ee648a5421a0
Luthfi Admin <luthfi@areshaflorist.com>
2026-01-06 14:42:17 +0700

    deploy fix

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -12,6 +12,7 @@ class Order extends Model
         'shipping_cost',
         'shipping_method',
         'status',
+        'payment_status',
         'customer_name',
         'customer_phone',
         'customer_address',
